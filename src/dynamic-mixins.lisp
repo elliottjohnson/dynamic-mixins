702a25df3b1e06baf3f15d15526e0411a682b7ef
Elliott Johnson <elliott@elliottjohnson.net>
2018-09-04 21:29:19 -0700

    Revert "Adding a feature to control default print-object behavior."

--- src/dynamic-mixins.lisp
+++ src/dynamic-mixins.lisp
@@ -14,6 +14,15 @@
               (or (class-name o) 'mixin-class)
               (mapcar #'class-name classes)))))
 
+(defclass mixin-object () ())
+(defmethod print-object ((o mixin-object) s)
+  (let ((c (class-of o)))
+    (with-slots (classes) c
+      (print-unreadable-object (o s :identity t)
+        (format s "~S ~S"
+                (or (class-name c) 'mixin-object)
+                (mapcar #'class-name classes))))))
+
 (defstruct mix-list (list nil))
 
 (defun %find-class (name-or-class)
